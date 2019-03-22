672b75be5061dd6f365c92e356c64f4871edc1a4
Charlie Wang <chwa@chwa-macbookpro.roam.corp.google.com>
2019-03-23 00:53:01 +0100

    review comments

--- source-code/build-pipeline/wait_for_dag_deployed.sh
+++ source-code/build-pipeline/wait_for_dag_deployed.sh
@@ -17,13 +17,13 @@
 # limitations under the License.
 
 n=0
-until [ $n -ge $4 ]
+until [[ $n -ge $4 ]]
 do
   status=0
-  gcloud composer environments run $1 --location $2 list_dags 2>&1 >/dev/null \
-  | grep $3 && break
+  gcloud composer environments run "${1}" --location "${2}" list_dags 2>&1 >/dev/null \
+  | grep "${3}" && break
   status=$?
-  n=$[$n+1]
-  sleep $5
+  n=$(($n+1))
+  sleep "${5}"
 done
 exit $status
